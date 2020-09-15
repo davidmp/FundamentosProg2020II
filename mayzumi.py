cb54f99b5ae0e767978dc8a8b213cab2af5d48a5
repl.it user <replituser@example.com>
2020-09-15 17:03:02 +0000

    Cambios en archivo

--- mayzumi.py
+++ mayzumi.py
@@ -1,5 +1,11 @@
-hello
-a=12
-b=30
-c=a+b
-print("resultado suma",c)
+print("hola mundo")
+a=6
+b=4
+r=a+b
+print("Suma: " ,r)
+r=a-b
+print("Resta:" ,r)
+r=a*b
+print("Multiplicacion:" ,r)
+r=a/b
+print:("Division:" ,r)
