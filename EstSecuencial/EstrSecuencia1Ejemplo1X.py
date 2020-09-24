55b019bd37371ba025bbc8dc0a928e821dc49ad4
davidmp <mamanipari@gmail.com>
2020-09-24 08:55:10 -0500

    Ejem. generado PSint Puthon

---
+++ EstSecuencial/EstrSecuencia1Ejemplo1X.py
@@ -0,0 +1,22 @@
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	n1 = float()
+	n2 = float()
+	n3 = float()
+	n4 = float()
+	pc = float()
+	print("Ingrese las 4 notas:")
+	# Datos de Entrada  
+	n1 = float(input())
+	n2 = float(input())
+	n3 = float(input())
+	n4 = float(input())
+	# Proceso  
+	pc = (n1+n2+n3+n4)/4
+	# Datos de Salida  
+	print("La calificación Promedio de notas es:",pc)
+
