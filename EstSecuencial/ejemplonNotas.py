ed880ceeecca74e3446ab8b86994e9ca019765c7
repl.it user <replituser@example.com>
2020-09-24 13:36:44 +0000

    ejemplo de notas

---
+++ EstSecuencial/ejemplonNotas.py
@@ -0,0 +1,9 @@
+def promedio_de_notas():
+ n1= int(input ("ingrese la primera nota:") )
+ n2= int(input ("ingrese la segunda nota:") )
+ n3= int(input ("ingrese la tercera nota:") )
+ n4= int(input ("ingrese la cuarta  nota:") )
+ pc=(n1+n2+n3+n4)/4
+ print ("la nota final es : ",pc) 
+
+promedio_de_notas()
