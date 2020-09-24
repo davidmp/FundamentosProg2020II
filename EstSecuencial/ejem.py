3d7890a3ca4d3f969e9c55482d985ae769934902
repl.it user <replituser@example.com>
2020-09-24 13:29:16 +0000

    ejem.py

---
+++ EstSecuencial/ejem.py
@@ -0,0 +1,12 @@
+def CalcularPromedioNota():
+  #Datos entrada
+  N1=int(input("Ingrese Nota 1:"))
+  N2=int(input("Ingrese Nota 2:"))
+  N3=int(input("Ingrese Nota 3:"))
+  N4=int(input("Ingrese Nota 4:"))
+  #Proceso
+  pc=(N1+N2+N3+N4)/4
+  #Datos de Salidad
+  print("La calificación de nota promedio es:",pc)
+
+  CalcularPromedioNota()
