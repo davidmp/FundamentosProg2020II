61c0c92aaee2512727e18831e2129aa13183e338
davidmp <mamanipari@gmail.com>
2020-11-05 10:10:58 -0500

    Proyecto GUI

---
+++ ProyectoUPeUGUI/src/main/java/pe/edu/upeu/app/App.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
