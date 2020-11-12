dd730d52f1159ea1ca5d55091cb7efde12039851
davidmp <mamanipari@gmail.com>
2020-11-12 10:32:11 -0500

    Ejemplo reportes

--- ProyectoUPeUGUI/src/main/java/pe/edu/upeu/app/App.java
+++ ProyectoUPeUGUI/src/main/java/pe/edu/upeu/app/App.java
@@ -3,6 +3,7 @@ package pe.edu.upeu.app;
 import java.io.Console;
 
 import pe.edu.upeu.core.FibonaciMain;
+import pe.edu.upeu.core.Reportes;
 import pe.edu.upeu.gui.MainGUI;
 import pe.edu.upeu.utils.LeerTeclado;
 
@@ -15,21 +16,24 @@ public class App{
     static LeerTeclado teclado=new LeerTeclado();
     static Console cons=System.console();
     static FibonaciMain fib=new FibonaciMain();
+    static Reportes rep=new Reportes();
 
     static void menuOpciones(){
         System.out.println( "*********************Bienvenidos al Sistema********************" );
         
         char opcion='S';
-        int numeroAloritmo=1;
+        int numeroAlgoritmo=1;
         do{
-            String datos="Seleccion la opción que desea:\n "+
+            String datos="Seleccion la opción que desea:\n"+
             "1=Algoritmo Fibonci Normal \n"+
-            "2=Finonaci Entorno Escritorio \n";
+            "2=Finonaci Entorno Escritorio \n"+
+            "3=Imprimir Productos \n";
 
-            numeroAloritmo=teclado.leer(0, datos);
-            switch(numeroAloritmo){
+            numeroAlgoritmo=teclado.leer(0, datos);
+            switch(numeroAlgoritmo){
                 case 1: System.out.println(fib.fibonaci(4));;break;
                 case 2: new MainGUI();break;
+                case 3: rep.imprimirMatriz(rep.reporteDatos());break;
                 default : System.out.println("La opción que selecciono no existe"); 
             }
             opcion=teclado.leer(' ', "Desea probar otras opciones? SI=S, NO=N");
--- ProyectoUPeUGUI/src/main/java/pe/edu/upeu/core/Reportes.java
+++ ProyectoUPeUGUI/src/main/java/pe/edu/upeu/core/Reportes.java
@@ -9,9 +9,16 @@ public class Reportes {
             {2, "Laptop", 10, 1500, "Samsung"},
             {3, "Celular", 20, 560.50, "Huawei"},
             {4, "Celular", 20, 560.50, "Huawei"}
-        };
-        
+        };        
         return data;
+    }
+
+    public void imprimirMatriz(Object[][] data){
+        String[] columnas=new String[] {"ID", "Producto", "Cantidad", "Precio", "Marca"};
+        System.out.println(columnas[0]+"\t"+columnas[1]+"\t"+columnas[2]+"\t"+columnas[3]+"\t"+columnas[4]);
+        for (Object[] objects : data) {
+            System.out.println(objects[0]+"\t"+objects[1]+"\t"+objects[2]+"\t"+objects[3]+"\t"+objects[4]);
+        }
     }
 
 }
