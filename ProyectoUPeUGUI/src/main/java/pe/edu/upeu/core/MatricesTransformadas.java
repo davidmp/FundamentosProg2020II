b39e15f729c4801b637f64f51b9b51f7a8014133
davidmp <mamanipari@gmail.com>
2020-12-15 14:03:49 +0000

    Transformada 35

--- ProyectoUPeUGUI/src/main/java/pe/edu/upeu/core/MatricesTransformadas.java
+++ ProyectoUPeUGUI/src/main/java/pe/edu/upeu/core/MatricesTransformadas.java
@@ -134,12 +134,35 @@ public int[][] transformada23(int dimen, int numInit){
     return matriz;
 }
 
+public int[][] transformada35(int dimen, int numInit){
+    int[][] matriz=new int[dimen][dimen];
+    int x, x1, x2, y, y1;
+    for(x=matriz.length-1; x>=matriz.length/2; x--){
+        for(y=(dimen-1)-x;y<=x;y++){
+            matriz[x][y]=numInit;
+            numInit++;
+        }
+        for(x1=y-2;x1>=(dimen-1)-x;x1--){
+            matriz[x1][x]=numInit;
+            numInit++;
+        }   
+        for(y1=y-2;y1>=(dimen-1)-x;y1--){
+            matriz[x1+1][y1]=numInit;
+            numInit++;
+        }   
+        for(x2=y1+2;x2<y-1;x2++){
+            matriz[x2][y1+1]=numInit;
+            numInit++;
+        }                    
+    }
 
+    return matriz;
+}
 
 
 public static void main(String[] args) {
    MatricesTransformadas mt=new MatricesTransformadas(); 
-   mt.ia.imprimirMatriz(mt.transformada23(5, 0));
+   mt.ia.imprimirMatriz(mt.transformada35(10, 0));
 }
 
     
